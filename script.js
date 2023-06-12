09ca2c404b57c35e6bd00533fbdc1e8cf6b4bab6
a32010 <a32010@tomazpelayo.com>
2023-06-12 12:24:17 +0100

    w5 update
    Major Updates to the Structure, Design and Functionality of the website.
    Addition of files and content.

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-window.addEventListener('beforeunload', function() {window.scrollTo(0, 0);});
+window.addEventListener('beforeunload', function() {
+    window.scrollTo(0, 0);
+});
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('logo-img').addEventListener('click', function() {location.reload();});
+    document.getElementById('logo-img').addEventListener('click', function() {
+        location.reload();
+    });
     document.getElementById('menu-icon').addEventListener('click', menu);
-    scrollIt();
+    menuIt();
 });
 
 function menu() {
-    var icon = document.getElementById('menu-icon');
-    var list = document.getElementById('menu');
-    var computedStyle = window.getComputedStyle(list);
-  
-    if (computedStyle.visibility === "hidden") {
-        icon.style.animation = "rotateRight 0.2s ease-in-out forwards";
-        list.style.visibility = "visible";
-        list.style.transformOrigin = "top center";
-        list.style.animation = "growDown 0.2s ease-in-out forwards";
-        setTimeout(function() {
-            list.style.transition = "right 0.2s";
-            list.style.right = "-10px"
-        }, 250);
+    const icon = document.getElementById('menu-icon');
+    const list = document.getElementById('menu-list');
+    const computedStyle = window.getComputedStyle(list);
+
+    if (computedStyle.visibility === 'hidden') {
+        icon.style.animation = "rotateLeft 0.1s ease-in-out forwards";
+        list.style.visibility = 'visible';
+        list.style.animation = "growX 0.1s ease-in-out forwards";
     } else {
-        icon.style.animation = "rotateLeft 0.2s ease-in-out forwards";
-        list.style.transition = "right 0.2s";
-        list.style.right = "-195px"
+        icon.style.animation = "rotateRight 0.1s ease-in-out forwards";
+        list.style.animation = "shrinkX 0.1s ease-in-out forwards";
         setTimeout(function() {
-            list.style.animation = "shrinkUp 0.2s ease-in-out forwards";
-            list.style.transformOrigin = "top center";
-            setTimeout(function() {list.style.visibility = "hidden";}, 200)
-        }, 250);
+            list.style.visibility = 'hidden'; 
+        }, 100);
     }
 }
 
-function scrollIt() {
+function menuIt() {
     const menuItems = document.getElementsByClassName('menu-item');
     
     menuItems[0].addEventListener('click', scrollToTop);
     
-    for (let i = 1; i < menuItems.length - 2; i++) {
+    for (let i = 1; i < menuItems.length - 1; i++) {
       menuItems[i].addEventListener('click', scrolling);
       menuItems[i].querySelector('i').addEventListener('click', scrolling);
       menuItems[i].querySelector('p').addEventListener('click', scrolling);
     }
-  
+
+    menuItems[menuItems.length - 1].addEventListener('click', function() {
+        location.reload();
+    })
+
     function scrolling(event) {
       const target = event.target.closest('.menu-item');
       document.getElementById(target.getAttribute('data-target')).scrollIntoView({ behavior: 'smooth' });
@@ -51,4 +51,4 @@ function scrollIt() {
     function scrollToTop() {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
-}  
+}
